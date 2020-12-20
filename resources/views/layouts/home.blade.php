c11abbb512fb8529423fb3d38808ed0f2f48b14e
niyazimuhammetkose <mxready@windowslive.com>
2020-12-20 16:55:02 +0300

    template

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,36 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>@yield('title')</title>
+    <title> @yield('title') </title>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <meta name="description" content=" @yield('description') ">
+    <meta name="keywords" content=" @yield('keywords') ">
+    <meta name="author" content="Alex de Souza">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/styles/bootstrap4/bootstrap.min.css">
+    <link href="{{ asset('assets') }}/plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/plugins/OwlCarousel2-2.2.1/animate.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/plugins/slick-1.8.0/slick.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/styles/main_styles.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/styles/responsive.css">
+
     @yield('css')
-    @yield('js')
+    @yield('headerjs')
+
 </head>
+
 <body>
+@include('home._header')
 
-<div class="jumbotron text-center">
-    <h1>Header</h1>
-    <p>Resize this responsive page to see the effect!</p>
-    <a href="/">Home</a>
-    <a href="/aboutus">About Us</a>
-    @yield('header')
-</div>
 
-<div class="container">
-    <div class="row">
-        @yield('sidebar')
-        @yield('content')
-    </div>
-</div>
 
-<div class="jumbotron text-center">
-    <h1>Footer</h1>
-    <p>Sayfa altı</p>
-    @yield('footer')
-</div>
+@section('content')
+    İçerik Alanı
+@show
 
+@include('home._footer')
+@yield('footerjs')
 </body>
+
 </html>
