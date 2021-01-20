94a4c70bd3f35adc2431f596429de6164a1d27ab
niyazimuhammetkose <mxready@windowslive.com>
2021-01-21 02:18:12 +0300

    user profil photo

--- routes/web.php
+++ routes/web.php
@@ -84,6 +84,12 @@ Route::middleware('auth')->prefix('myaccount')->namespace('myaccount')->group(fu
 
 });
 
+Route::middleware('auth')->prefix('user')->namespace('user')->group(function (){
+
+    Route::get('/profile', [\App\Http\Controllers\UserController::class, 'index'])->name('userprofile');
+
+});
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
